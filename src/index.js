e34903518ba2adb02813c031ee29aefa9f189d26
lucasdev123 <lucasafeng@gmail.com>
2020-12-10 00:30:03 -0300

    UTW

--- src/index.js
+++ src/index.js
@@ -13,17 +13,35 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET resquests are disabled')
-//     }else {
-//         next()
-//     }
-// })
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
+
+const multer = require('multer')
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'))
+        }
+
+        cb(undefined, true)
+        
+        // cb(new Error('File must be a PDF'))
+        // cb(undefined, true)
+
+    }
+})
+
+app.post('/upload', upload.single('upload'),(req, res) => {
+    console.log('entrou')
+    res.send()
+})
+
+
+
+
 
 app.use(express.json()) //prepares to receive req with json files
 
@@ -33,15 +51,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server up and running on port: ' + port)
 })
-
-const main = async () => {
-    // const task = await Task.findById('5fcef9cc94cfc81b10a95271')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('5fcef7b9acae930ac4c9357a')
-    await user.populate('tasks').execPopulate()
-    console.log(user)
-} 
-
-main()
